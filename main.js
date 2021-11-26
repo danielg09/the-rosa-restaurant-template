a1fb996276f9c73e686b16deb5c9e6857ddc3470
Daniel Murillo Sanchez <dgerardo94@gmail.com>
2021-11-26 14:44:28 -0600

    Se trabaja el header mobile design

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 //Select element function
+const menutogle = document.querySelector('.menu-toggle');
+const body = document.querySelector('body');
+
+menutogle.addEventListener('click', () =>{
+    body.classList.toggle('open');
+});
 
